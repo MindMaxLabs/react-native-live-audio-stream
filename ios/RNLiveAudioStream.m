7a15ae60891b646cb4649ea40b31374eefdc2714
Michał Sęk <senek@omaewa.eu>
2025-04-01 16:18:47 +0200

    improvement: remove dependency on AVAudioSession

--- ios/RNLiveAudioStream.m
+++ ios/RNLiveAudioStream.m
@@ -5,88 +5,56 @@
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(init:(NSDictionary *) options) {
-    RCTLogInfo(@"[RNLiveAudioStream] init");
-    _recordState.mDataFormat.mSampleRate        = options[@"sampleRate"] == nil ? 44100 : [options[@"sampleRate"] doubleValue];
-    _recordState.mDataFormat.mBitsPerChannel    = options[@"bitsPerSample"] == nil ? 16 : [options[@"bitsPerSample"] unsignedIntValue];
-    _recordState.mDataFormat.mChannelsPerFrame  = options[@"channels"] == nil ? 1 : [options[@"channels"] unsignedIntValue];
-    _recordState.mDataFormat.mBytesPerPacket    = (_recordState.mDataFormat.mBitsPerChannel / 8) * _recordState.mDataFormat.mChannelsPerFrame;
-    _recordState.mDataFormat.mBytesPerFrame     = _recordState.mDataFormat.mBytesPerPacket;
-    _recordState.mDataFormat.mFramesPerPacket   = 1;
-    _recordState.mDataFormat.mReserved          = 0;
-    _recordState.mDataFormat.mFormatID          = kAudioFormatLinearPCM;
-    _recordState.mDataFormat.mFormatFlags       = _recordState.mDataFormat.mBitsPerChannel == 8 ? kLinearPCMFormatFlagIsPacked : (kLinearPCMFormatFlagIsSignedInteger | kLinearPCMFormatFlagIsPacked);
-    _recordState.bufferByteSize                 = options[@"bufferSize"] == nil ? 2048 : [options[@"bufferSize"] unsignedIntValue];
-    _recordState.mSelf = self;
+  RCTLogInfo(@"[RNLiveAudioStream] init");
+  _recordState.mDataFormat.mSampleRate        = options[@"sampleRate"] == nil ? 44100 : [options[@"sampleRate"] doubleValue];
+  _recordState.mDataFormat.mBitsPerChannel    = options[@"bitsPerSample"] == nil ? 16 : [options[@"bitsPerSample"] unsignedIntValue];
+  _recordState.mDataFormat.mChannelsPerFrame  = options[@"channels"] == nil ? 1 : [options[@"channels"] unsignedIntValue];
+  _recordState.mDataFormat.mBytesPerPacket    = (_recordState.mDataFormat.mBitsPerChannel / 8) * _recordState.mDataFormat.mChannelsPerFrame;
+  _recordState.mDataFormat.mBytesPerFrame     = _recordState.mDataFormat.mBytesPerPacket;
+  _recordState.mDataFormat.mFramesPerPacket   = 1;
+  _recordState.mDataFormat.mReserved          = 0;
+  _recordState.mDataFormat.mFormatID          = kAudioFormatLinearPCM;
+  _recordState.mDataFormat.mFormatFlags       = _recordState.mDataFormat.mBitsPerChannel == 8 ? kLinearPCMFormatFlagIsPacked : (kLinearPCMFormatFlagIsSignedInteger | kLinearPCMFormatFlagIsPacked);
+  _recordState.bufferByteSize                 = options[@"bufferSize"] == nil ? 2048 : [options[@"bufferSize"] unsignedIntValue];
+  _recordState.mSelf = self;
 }
 
 RCT_EXPORT_METHOD(start) {
-    RCTLogInfo(@"[RNLiveAudioStream] start");
-    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-    NSError *error = nil;
+  RCTLogInfo(@"[RNLiveAudioStream] start");
+  _recordState.mIsRunning = true;
 
-    [audioSession setCategory: AVAudioSessionCategoryPlayAndRecord
-                                   mode: AVAudioSessionModeVoiceChat
-                                options: AVAudioSessionCategoryOptionAllowBluetooth |
-                                         AVAudioSessionCategoryOptionAllowAirPlay
-                                  error: &error];
+  OSStatus status = AudioQueueNewInput(&_recordState.mDataFormat, HandleInputBuffer, &_recordState, NULL, NULL, 0, &_recordState.mQueue);
 
-    if (error != nil) {
-        RCTLog(@"[RNLiveAudioStream] Problem setting up AVAudioSession category and mode. Error: %@", error);
-        return;
-    }
+  if (status != 0) {
+    RCTLog(@"[RNLiveAudioStream] Record Failed. Cannot initialize AudioQueueNewInput. status: %i", (int) status);
+    return;
+  }
 
-    [audioSession setActive:true error:&error];
+  for (int i = 0; i < kNumberBuffers; i++) {
+    AudioQueueAllocateBuffer(_recordState.mQueue, _recordState.bufferByteSize, &_recordState.mBuffers[i]);
+    AudioQueueEnqueueBuffer(_recordState.mQueue, _recordState.mBuffers[i], 0, NULL);
+  }
 
-    if (error != nil) {
-       RCTLog(@"[RNLiveAudioStream] Problem while activating AVAudioSEssion. Error: %@", error);
-       return;
-    }
-
-    [audioSession setAggregatedIOPreference:AVAudioSessionIOTypeAggregated error:&error];
-  
-    if (error != nil) {
-       RCTLog(@"[RNLiveAudioStream] Problem while setting aggregated IO preference. Error: %@", error);
-       return;
-    }
-
-    _recordState.mIsRunning = true;
-
-    OSStatus status = AudioQueueNewInput(&_recordState.mDataFormat, HandleInputBuffer, &_recordState, NULL, NULL, 0, &_recordState.mQueue);
-    if (status != 0) {
-        RCTLog(@"[RNLiveAudioStream] Record Failed. Cannot initialize AudioQueueNewInput. status: %i", (int) status);
-        return;
-    }
-
-    for (int i = 0; i < kNumberBuffers; i++) {
-        AudioQueueAllocateBuffer(_recordState.mQueue, _recordState.bufferByteSize, &_recordState.mBuffers[i]);
-        AudioQueueEnqueueBuffer(_recordState.mQueue, _recordState.mBuffers[i], 0, NULL);
-    }
-    AudioQueueStart(_recordState.mQueue, NULL);
+  AudioQueueStart(_recordState.mQueue, NULL);
 }
 
 RCT_EXPORT_METHOD(stop:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock) reject) {
-    if (_recordState.mIsRunning) {
-        RCTLogInfo(@"[RNLiveAudioStream] stop");
-        AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-        NSError *error = nil;
+  if (_recordState.mIsRunning) {
+    RCTLogInfo(@"[RNLiveAudioStream] stop");
 
-        _recordState.mIsRunning = false;
-        AudioQueueStop(_recordState.mQueue, true);
-        for (int i = 0; i < kNumberBuffers; i++) {
-            AudioQueueFreeBuffer(_recordState.mQueue, _recordState.mBuffers[i]);
-        }
-        AudioQueueDispose(_recordState.mQueue, true);
+    _recordState.mIsRunning = false;
+    AudioQueueStop(_recordState.mQueue, true);
 
-        [audioSession setActive:false error:&error];
-
-        if (error != nil) {
-            RCTLog(@"[RNLiveAudioStream] Problem while deactivating AVAudioSEssion. Error: %@", error);
-            reject(@"stop_failure", @"AVAudioSession not deactivated", nil);
-            return;
-        }
+    for (int i = 0; i < kNumberBuffers; i++) {
+      AudioQueueFreeBuffer(_recordState.mQueue, _recordState.mBuffers[i]);
     }
 
-    resolve(@"stopped");
+    AudioQueueDispose(_recordState.mQueue, true);
+  } else {
+    RCTLogInfo(@"[RNLiveAudioStream] stop called but not running");
+  }
+
+  resolve(@"stopped");
 }
 
 void HandleInputBuffer(void *inUserData,
@@ -95,28 +63,29 @@ void HandleInputBuffer(void *inUserData,
                        const AudioTimeStamp *inStartTime,
                        UInt32 inNumPackets,
                        const AudioStreamPacketDescription *inPacketDesc) {
-    AQRecordState* pRecordState = (AQRecordState *)inUserData;
+  AQRecordState* pRecordState = (AQRecordState *)inUserData;
 
-    if (!pRecordState->mIsRunning) {
-        return;
-    }
+  if (!pRecordState->mIsRunning) {
+      return;
+  }
 
-    short *samples = (short *) inBuffer->mAudioData;
-    long nsamples = inBuffer->mAudioDataByteSize;
-    NSData *data = [NSData dataWithBytes:samples length:nsamples];
-    NSString *str = [data base64EncodedStringWithOptions:0];
-    [pRecordState->mSelf sendEventWithName:@"data" body:str];
+  short *samples = (short *) inBuffer->mAudioData;
+  long nsamples = inBuffer->mAudioDataByteSize;
 
-    AudioQueueEnqueueBuffer(pRecordState->mQueue, inBuffer, 0, NULL);
+  NSData *data = [NSData dataWithBytes:samples length:nsamples];
+  NSString *str = [data base64EncodedStringWithOptions:0];
+
+  [pRecordState->mSelf sendEventWithName:@"data" body:str];
+  AudioQueueEnqueueBuffer(pRecordState->mQueue, inBuffer, 0, NULL);
 }
 
 - (NSArray<NSString *> *)supportedEvents {
-    return @[@"data"];
+  return @[@"data"];
 }
 
 - (void)dealloc {
-    RCTLogInfo(@"[RNLiveAudioStream] dealloc");
-    AudioQueueDispose(_recordState.mQueue, true);
+  RCTLogInfo(@"[RNLiveAudioStream] dealloc");
+  AudioQueueDispose(_recordState.mQueue, true);
 }
 
 @end
